9d19ab8392d028e87bdc7797e7e8b3987b945fac
Ritik303 <rtk3032002@gmail.com>
2025-01-30 11:42:02 +0530

    Changes

--- Q6_FindFactorial.java
+++ Q6_FindFactorial.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
 public class Q6_FindFactorial {
-    
+    public static int getInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+        return num;
+    }
+
+    public static long factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1; 
+        }
+        return n * factorial(n - 1); 
+    }
+    public static void displayResult(int num, long result) {
+        System.out.println("Factorial of " + num + " is: " + result);
+    }
+
+    public static void main(String[] args) {
+        int num = getInput(); 
+        long result = factorial(num); 
+        displayResult(num, result); 
+    }
 }
+
+
